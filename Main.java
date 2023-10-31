67660a474b5ad7ef3e588e2e1fe68294a496e8f4
silvaandre04 <148702384+silvaandre04@users.noreply.github.com>
2023-10-31 16:55:44 +0800

    Add files via upload
    FILE2 KONSTROKTOR

--- Main.java
+++ Main.java
@@ -1,23 +1,26 @@
 package com.tutorial;
 
-class mahasiswa {
-    String nama;
-    String nim;
-    String prodi;
+class Polos{
+    String dataString;
+    int dataInteger;
 }
 
+
 public class Main {
-   public static void main(String[] args) {
-      // instansiasi objek dari class Mahasiswa
-      mahasiswa mhs1 = new mahasiswa(); // proses instansiasi
 
-      //memberi inisisasi pada objek baru
-      mhs1.nama = "Wilson le siento";
-      mhs1.nim = "22241068";
-      mhs1.prodi = "teknik perampokan";
+    public static void main(String[] args) {
+        // instansi objek
+        Polos pls = new Polos();
 
-      System.out.println("Nama :"+mhs1.nama);
-      System.out.println("NIM :"+mhs1.nim);
-      System.out.println("Prodi :"+mhs1.prodi);
-   } 
+        // memberi nilai pada objek pls
+        pls.dataString = "Polos";
+        pls.dataInteger = 110;
+        System.out.println("dataString : " + pls.
+        dataString);
+        System.out.println("dataInteger : " + pls.
+        dataInteger);
+
+
+    
+    }
 }
